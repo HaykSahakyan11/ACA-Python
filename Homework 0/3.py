46b78262709b4e76f979d0248ffac2c09c106402
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-11 21:21:35 +0400

    new

--- Homework 0/3.py
+++ Homework 0/3.py
@@ -1,3 +1,2 @@
 a_string = "ada"
-print("False" if len([True for i in range(len(a_string) // 2) if a_string[i] == a_string[-1 - i]]) < len(
-    a_string) // 2 else "True")
+print("Yes" if a_string == a_string[::-1] else "No")
