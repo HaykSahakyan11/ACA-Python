94c468b8a538751af4514defb5d7cde6627c3204
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-11 21:11:09 +0400

    new

--- Homework 0/2.py
+++ Homework 0/2.py
@@ -1,4 +1,8 @@
+def newList(a_list):
+    new_list = []
+    [new_list.append(elem) for elem in a_list if elem not in new_list]
+    return new_list
+
+
 a_list = [[1, 2], [1, 2], [1, 2, 3]]
-new_list = []
-[new_list.append(elem) for elem in a_list if elem not in new_list]
-print(new_list)
+print(newList(a_list))
