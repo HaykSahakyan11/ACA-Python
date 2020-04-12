378f4c286605f0659951f2c3b1fccd72381d3e5f
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-12 17:45:15 +0400

    11

--- Homework 0/2.py
+++ Homework 0/2.py
@@ -1,7 +1,8 @@
 def newList(a_list):
-    new_list = []
-    [new_list.append(elem) for elem in a_list if elem not in new_list]
-    return new_list
+    for elem in a_list:
+        while a_list.count(elem) > 1:
+            a_list.remove(elem)
+    return a_list
 
 
 a_list = [[1, 2], [1, 2], [1, 2, 3]]
