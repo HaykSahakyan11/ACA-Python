d9a254043cdbe18035ee73e86f2b944d7e05e912
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-12 19:11:51 +0400

    11

--- Homework 0/2.py
+++ Homework 0/2.py
@@ -1,3 +1,8 @@
+def newList1(a_list):
+    [a_list.remove(elem) for elem in a_list if a_list.count(elem) > 1]
+    return a_list
+
+
 def newList(a_list):
     for elem in a_list:
         while a_list.count(elem) > 1:
@@ -6,4 +11,5 @@ def newList(a_list):
 
 
 a_list = [[1, 2], [1, 2], [1, 2, 3]]
+print(newList1(a_list))  # short
 print(newList(a_list))
