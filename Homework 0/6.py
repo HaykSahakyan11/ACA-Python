82f0d4440fa049a7d0b3955000f86ae19fa33904
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-12 16:44:55 +0400

    11

--- Homework 0/6.py
+++ Homework 0/6.py
@@ -1,23 +1,22 @@
 a, b = 10, 15
 
 
+def euclid_algo(a, b):
+    a, b = (a - b, b) if a > b else (a, b - a)
+    return euclid_algo(a, b) if a != b else a
+
+
+# more effective than euclid_algorithm
+def m_div(a, b):
+    if b > 0: a, b = b, a % b
+    return m_div(a, b) if b > 0 else a
+
+
 # comprehension
 def max_div(a, b):
     return [i for i in range(min(a, b), 0, -1) if a % i == 0 and b % i == 0][0]
 
 
-# recursion
-def max_divisor(a, b):
-    if b > 0:
-        a, b = b, a % b
-        return max_divisor(a, b)
-    return a
-
-
-# same as above function but gives error
-def n_div(a, b):
-    return n_div(lambda a, b: b, a % b) if b > 0 else a
-
-
 # print(max_div(a, b)) #comprehension
-print(max_divisor(a, b))  # recursion
+# print(euclid_algo(a, b))
+print(m_div(a, b))
