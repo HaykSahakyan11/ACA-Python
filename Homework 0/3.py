2eb56ad1d871f223a3eb1f2f55a512615688ab8f
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-14 15:40:06 +0400

    11

--- Homework 0/3.py
+++ Homework 0/3.py
@@ -1,2 +1,10 @@
+def is_simmetric(a_string):
+    for i in range(len(a_string) // 2):
+        if a_string[i] != a_string[-1 - i]:
+            return False
+    return True
+
+
 a_string = "ada"
-print("Yes" if a_string == a_string[::-1] else "No")
+print(is_simmetric(a_string))
+# print("Yes" if a_string == a_string[::-1] else "No")
