c80f761b4bf97e890e5dfe6c42068ce5279c7e4d
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-16 17:50:17 +0400

    16

--- Practical/7.py
+++ Practical/7.py
@@ -6,18 +6,10 @@ def isCircleShifted(list1, list2):
     # we can find it by taking 1st elem (index [0]) in list1 and checking what happens with it in list2 (list2.index(list1[0])).
     # And if we shift all elements of list1 by same step and get that shifted list1 is equal to list2 return True
     step = list2.index(list1[0])
-    # if step is negative -> shifted left, we shift all elems by step to appropriate direction
-    if step < 0:
-        step = abs(step)
-        for i in range(step):
-            list1.append(list1.pop(0))
-    # if step is positive -> shifted right
-    else:
-        for i in range(step):
-            list1.insert(0, list1.pop())
-    # Final comparison-> shifted right, we shift all elems by step to appropriate direction
+    for i in range(step):
+        list1.insert(0, list1.pop())
     return list1 == list2
 
 
-list1, list2 = [1, 3, 4, 5, 2], [5, 2, 6, 3, 4]
+list1, list2 = [1, 3, 4, 5, 2], [5, 2, 1, 3, 4]
 print(isCircleShifted(list1, list2))
