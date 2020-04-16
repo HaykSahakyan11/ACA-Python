927cf6d5ebc459c5714f1b6723a7e40f38dc367f
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-16 16:23:15 +0400

    16

--- Practical/8.py
+++ Practical/8.py
@@ -9,5 +9,26 @@ def maxSecNumber(a_list):
     return 0.5 if max2 == float('-inf') or max1 == max2 else max2
 
 
+
+
+def maximumNumber(a_list,maxnum = None):
+    for i in range(len(a_list)):
+        if  maxnum == None or a_list[i] > maxnum:
+            maxnum = a_list[i]
+    return maxnum
+
+def maxWithSet(a_list):
+    a_list = list(set(a_list))
+    if len(a_list) < 2:
+        return 0.5
+    maxnum1 = maximumNumber(a_list)
+    a_list.remove(maxnum1)
+    return maximumNumber(a_list)
+
+
+
+
+
 a_list = [5, 5, 1, 4, 5]
-print(maxSecNumber(a_list))
+#print(maxSecNumber(a_list))
+print(maxWithSet(a_list))
