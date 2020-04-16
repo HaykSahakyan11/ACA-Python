92d8b1ed1fdfb621d7ef70b8b137e9bda82235eb
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-16 14:51:50 +0400

    16

--- Practical/4.py
+++ Practical/4.py
@@ -2,4 +2,14 @@ def factorial(a_number):
     return 1 if a_number == 1 or a_number == 0 else a_number * factorial(a_number - 1)
 
 
-print(factorial(5))
+
+def fac_For_loop(a_number):
+    factor = 1
+    for i in range(1,a_number+1):
+        factor *= i
+    return factor
+
+print(factorial(3))
+print(fac_For_loop(3))
+
+
