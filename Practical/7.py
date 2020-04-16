c16adf45070e9a6033ae49a3a9ec5ba0c8edf0b1
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-16 16:23:42 +0400

    16

--- Practical/7.py
+++ Practical/7.py
@@ -1,4 +1,6 @@
 def isCircleShifted(list1, list2):
+    if set(list1) != set(list2):
+        return False
     # Function gives True  if all elements in list1 shifted by same number or step otherwise False
     # Assumes if there is a pattern of moving (step),
     # we can find it by taking 1st elem (index [0]) in list1 and checking what happens with it in list2 (list2.index(list1[0])).
@@ -17,5 +19,5 @@ def isCircleShifted(list1, list2):
     return list1 == list2
 
 
-list1, list2 = [1, 3, 4, 5, 2], [5, 2, 1, 3, 4]
+list1, list2 = [1, 3, 4, 5, 2], [5, 2, 6, 3, 4]
 print(isCircleShifted(list1, list2))
