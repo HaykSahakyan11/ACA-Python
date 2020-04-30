90d3d145d7e773905e3eb02e368b993d0d41b418
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-30 16:07:32 +0400

    11

--- WEB/server.py
+++ WEB/server.py
@@ -8,32 +8,28 @@ def parse_ini(config_path="config.ini"):
     with open(config_path, "r") as ini:
         section = None
         for num_line, line in enumerate(ini, 1):
-            line = line.strip()
             if line != "":
-                if line.startswith("["):
-                    if line.endswith("]") and line.count("[") == line.count("]"):
-                        section = line.strip()[1:-1]
-                    else:
-                        return "Something wrong with brackets in {} line".format(line)
+                line = line.strip()
+                if line.startswith("[") and line.endswith("]"):
+                    section = line[1:-1]
                 else:
                     if section == None:
-                        return "File contains no section headers"
+                        raise ValueError("File contains no section headers")
                     else:
-                        line = line.replace(" ", "")
                         try:
-                            a = line[line.index("=") + 1:]
+                            key, value = (line[:line.index("=")]).strip(), (line[line.index("=") + 1:]).strip()
                             try:
-                                if float(a).is_integer():
-                                    a = int(a)
+                                if float(value).is_integer():
+                                    value = int(value)
                             except:
                                 pass
-                            d[section][line[:line.index("=")]] = a
-                        except:
-                            return "There is not '=' in the {} line {} section".format(line, section)
+                            d[section][key] = value
+                        except ValueError as error:
+                            return "ValueError: '=' {} in the {} line {} section".format(error, line, section)
         return dict(d)
 
 
-now = datetime.now()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     config = parse_ini()
     sock.bind((config["inet"]["ip"], config["inet"]["port"]))
@@ -47,6 +43,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             data = data.decode("utf-8")
             input_dict = {"exit": "Goodbye", "day": "%d", "month": "%m", "year": "%Y", "time": "%H:%M"}
             if data in input_dict:
+                if data == "exit":
+                    print("Disconnected")
+                    break
+                now = datetime.now()
                 conn.sendall(now.strftime("{}".format(input_dict[data])).encode("utf-8"))
             else:
                 conn.sendall("Unrecognized command {}".format(data).encode("utf-8"))
