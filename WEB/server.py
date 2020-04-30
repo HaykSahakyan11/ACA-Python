25f3b61bca92c86aff2b9cecffa91247c6c097a6
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-04-30 16:41:49 +0400

    11

--- WEB/server.py
+++ WEB/server.py
@@ -8,8 +8,8 @@ def parse_ini(config_path="config.ini"):
     with open(config_path, "r") as ini:
         section = None
         for num_line, line in enumerate(ini, 1):
+            line = line.strip()
             if line != "":
-                line = line.strip()
                 if line.startswith("[") and line.endswith("]"):
                     section = line[1:-1]
                 else:
@@ -43,10 +43,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             data = data.decode("utf-8")
             input_dict = {"exit": "Goodbye", "day": "%d", "month": "%m", "year": "%Y", "time": "%H:%M"}
             if data in input_dict:
+                now = datetime.now()
+                conn.sendall(now.strftime("{}".format(input_dict[data])).encode("utf-8"))
                 if data == "exit":
                     print("Disconnected")
                     break
-                now = datetime.now()
-                conn.sendall(now.strftime("{}".format(input_dict[data])).encode("utf-8"))
             else:
                 conn.sendall("Unrecognized command {}".format(data).encode("utf-8"))
