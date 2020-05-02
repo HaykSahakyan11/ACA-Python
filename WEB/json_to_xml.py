ee030ab9652e0a68566ab91606d2e4965ec36499
Hayk Sahakyan <hayk.ashoti.sahakyan@gmail.com>
2020-05-02 15:27:27 +0400

    11

--- WEB/json_to_xml.py
+++ WEB/json_to_xml.py
@@ -1,10 +1,16 @@
 import json
-with open("storage.json","r") as js_file:
-    decoded_json = json.load(js_file)
 
-xml = []
-for i in range(len(decoded_json)):
-    for key, value in decoded_json[i].items():
-        xml.append("<{0}>{1}</{0}>".format(key,value))
 
-print(xml)
+def json_to_xml(path):
+    with open(path, "r") as js_file:
+        decoded_json = json.load(js_file)
+    xml = []
+    for i in range(len(decoded_json)):
+        xml.append("<{}>".format(i))
+        for key, value in decoded_json[i].items():
+            xml.append("<{0}>{1}</{0}>".format(key, value))
+        xml.append("</{}>".format(i))
+    return "\n".join(xml)
+
+
+print(json_to_xml("storage.json"))
